74024468cd35f2413644783efa9e83f293349830
Alan Pieczonka <alanpieczonka@gmail.com>
2017-09-16 15:56:25 +0200

    Full props and profile picture styles

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,62 +4,62 @@ import { Container, Table } from 'reactstrap';
 
 import axios from 'axios';
 
-import  { SelectInput }  from './SelectInput';
+import { SelectInput } from './SelectInput';
 import { TableHead } from './TableHead';
 import { TableRow } from './TableRow';
 
 class Main extends Component {
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        oneMonth: [],
-        selectedDaysState: '30days' 
+  constructor(props) {
+    super(props);
+    this.state = {
+      oneMonth: [],
+      selectedDaysState: '30days'
     };
-} 
-
-  getInitialState(){
-
-
-  }
-  
-  componentWillMount() {
-
-    axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
-    .then(({ data }) => {
-      this.setState({ oneMonth: data });
-      console.log(this.state.oneMonth);
-      console.log(this.state.oneMonth[0]);
-      console.log(this.state.oneMonth[0].username);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
   }
 
-  componentDidUpdate(prevProps, prevState){
-    console.log(this.state.selectedDaysState);
-}
+  getInitialState() {}
 
-    handleSelect = (event) =>{
-      this.setState({
-        selectedDaysState: event.target.value
+  componentDidMount() {
+    axios
+      .get('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+      .then(({ data }) => {
+        this.setState({ oneMonth: data });
+        console.log(this.state.oneMonth);
+        console.log(this.state.oneMonth[0]);
+        console.log(this.state.oneMonth[0].username);
+      })
+      .catch(function(error) {
+        console.log(error);
       });
-    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(this.state.selectedDaysState);
+  }
+
+  handleSelect = event => {
+    this.setState({
+      selectedDaysState: event.target.value
+    });
+  };
 
   render() {
     return (
       <Container>
-        <SelectInput onChange={this.handleSelect}/>
+        <SelectInput onChange={this.handleSelect} />
         <Table>
           <TableHead />
           <tbody>
-              {
-                this.state.oneMonth.map((row, index) => (
-                  <TableRow key={index} index={index} username={this.state.oneMonth[index].username} monthpoints={this.state.oneMonth[index].recent} allpoints={this.state.oneMonth[index].alltime}/>
-                ))
-              }
+            {this.state.oneMonth.map((row, index) => (
+              <TableRow
+                key={index}
+                index={index}
+                username={this.state.oneMonth[index].username}
+                profilepicture={this.state.oneMonth[index].img}
+                monthpoints={this.state.oneMonth[index].recent}
+                allpoints={this.state.oneMonth[index].alltime}
+              />
+            ))}
           </tbody>
         </Table>
       </Container>
--- src/Components/TableRow.js
+++ src/Components/TableRow.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { FormGroup, Label, Input } from 'reactstrap';
 
+import customTableRow from './../css/ComponentsStyles/customTableRow.css';
+
 export const TableRow = (props) =>{
     return(
      <tr>
         <th scope="row">{props.index + 1}</th>
         <td>{props.username}</td>
-        <td>Img tag</td>
+        <td><img src={props.profilepicture} className="profilePicture" alt="Camper Profile Picture"/></td>
         <th>{props.monthpoints}</th>
         <th>{props.allpoints}</th>
       </tr>
