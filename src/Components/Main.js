a0a2798d0bd3b1b6131df517ba8929b844eeb2c1
Alan Pieczonka <alanpieczonka@gmail.com>
2017-09-16 16:13:48 +0200

    App works, needs a lot of refactoring and style fixes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -13,6 +13,7 @@ class Main extends Component {
     super(props);
     this.state = {
       oneMonth: [],
+      allTime: [],
       selectedDaysState: '30days'
     };
   }
@@ -31,6 +32,19 @@ class Main extends Component {
       .catch(function(error) {
         console.log(error);
       });
+
+      axios
+      .get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+      .then(({ data }) => {
+        this.setState({ allTime: data });
+        console.log(this.state.allTime);
+        console.log(this.state.allTime[0]);
+        console.log(this.state.allTime[0].username);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -44,22 +58,38 @@ class Main extends Component {
   };
 
   render() {
+
+    if(this.state.selectedDaysState === '30days'){          
+      var testingComp =  this.state.oneMonth.map((row, index) => (
+        <TableRow
+          key={index}
+          index={index}
+          username={this.state.oneMonth[index].username}
+          profilepicture={this.state.oneMonth[index].img}
+          monthpoints={this.state.oneMonth[index].recent}
+          allpoints={this.state.oneMonth[index].alltime}
+        />
+      ))
+    }else if(this.state.selectedDaysState === 'alltime'){
+      var testingComp =  this.state.allTime.map((row, index) => (
+        <TableRow
+          key={index}
+          index={index}
+          username={this.state.allTime[index].username}
+          profilepicture={this.state.allTime[index].img}
+          monthpoints={this.state.allTime[index].recent}
+          allpoints={this.state.allTime[index].alltime}
+        />
+      ))
+    }
+
     return (
       <Container>
         <SelectInput onChange={this.handleSelect} />
         <Table>
           <TableHead />
-          <tbody>
-            {this.state.oneMonth.map((row, index) => (
-              <TableRow
-                key={index}
-                index={index}
-                username={this.state.oneMonth[index].username}
-                profilepicture={this.state.oneMonth[index].img}
-                monthpoints={this.state.oneMonth[index].recent}
-                allpoints={this.state.oneMonth[index].alltime}
-              />
-            ))}
+          <tbody>   
+            {testingComp}
           </tbody>
         </Table>
       </Container>
